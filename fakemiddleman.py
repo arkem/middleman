3935afc2a36c2dbc6743cff099efd19b371f64c9
Arkem <arkem@mornmist.com>
2010-07-26 16:57:39 -0700

    Switched to tweepy and implemented OAuth

--- fakemiddleman.py
+++ fakemiddleman.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-import twitter
 import time
 import re
 import random
+import tweepy
 
 random.seed()
+
+# CREDENTIALS FOR REFERENCE "fakemiddleman", "crooked0little0vein"
+CONSUMER_KEY = "cdr4KBB6F4XkQaOtaXOpg"
+CONSUMER_SECRET = "ck7gIc4hgz6wzOoKeWa2gm3FiGmDfy4rXfQumiA"
+TOKEN_KEY = '123086520-opu1goU3YNNVAM3Mjme9jYV2F1zIZt3CBo0D0oUY'
+TOKEN_SECRET = '0C3xTHJKjdKjvwJTjwF9z06KePp5YoS67OVy2SqLhx8'
 
 MIN_WAIT = 60 # In minutes
 MAX_WAIT = 6 * 60
@@ -15,37 +21,34 @@ MIDDLEMAN_TWEETS = open("middleman.txt").readlines()
 
 def get_name(api):
     global MIDDLEMAN_FOLLOWERS, SPAMMERS
-    num_followers = api.users.show(screen_name = 'fakemiddleman')
+    num_followers = api.me().followers_count
     if num_followers != (len(MIDDLEMAN_FOLLOWERS) + SPAMMERS):
         MIDDLEMAN_FOLLOWERS = []
         SPAMMERS = 0
-        next_cursor = -1
-        while next_cursor != 0:
-            res = api.statuses.followers(screen_name='fakemiddleman',\
-                                         cursor=next_cursor)
-            for u in res['users']:
-                # Basic spammer filter need a better than 1:4 follow ratio
-                if float(u['followers_count']) / u['friends_count'] > 0.25:
-                    MIDDLEMAN_FOLLOWERS.append(u['screen_name'])
-                else:
-                    SPAMMERS += 1
-            next_cursor = res['next_cursor']
-            print next_cursor, type(next_cursor)
+        for f in api.followers():
+            # Basic spammer filter need a better than 1:4 follow ratio
+            if float(f.followers_count / f.friends_count) > 0.25:
+                MIDDLEMAN_FOLLOWERS.append(f.screen_name)
+            else:
+                SPAMMERS += 1
     if len(MIDDLEMAN_FOLLOWERS) == 0:
         return "Dubbie"
     return random.choice(MIDDLEMAN_FOLLOWERS)
-    
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(TOKEN_KEY, TOKEN_SECRET)
+api = tweepy.API(auth)
 
 while (True):
     try:
         post = random.choice(MIDDLEMAN_TWEETS).strip()
-        api = twitter.Twitter("fakemiddleman", "crooked0little0vein")
         name = get_name(api)
         post = post.replace('{$NAME}', "@%s" % name)
         print post, name, len(MIDDLEMAN_TWEETS), SPAMMERS
-        api.statuses.update(status = post)
+        api.update_status(post)
         print "success"
-    except Exception:
+    except Exception as e:
+        print e
         pass
     sleepinterval = random.randrange(MIN_WAIT, MAX_WAIT)
     print "Sleeping for %d minutes" % sleepinterval 
